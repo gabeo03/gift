8eb3f2caca0dfa3cda50ff52e543697f3295dc1f
xuanphong1001 <trinhxuanphong100103@gmail.com>
2026-03-05 13:36:51 +0700

    fixx 8

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
-  server: { port: 3000, open: true },
-  build:  { outDir: 'dist' },
-});
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        win:  resolve(__dirname, 'win.html'),
+        lose: resolve(__dirname, 'lose.html'),
+      }
+    }
+  }
+})
